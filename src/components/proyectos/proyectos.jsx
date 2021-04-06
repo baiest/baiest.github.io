3ff6c4109f85d1bc5960577a1074627df8da8fed
baiest <ballesteros.juan@correounivalle.edu.co>
2021-04-06 16:12:31 -0500

    Ultimos cambios para adaptar pantalla

--- src/components/proyectos/proyectos.jsx
+++ src/components/proyectos/proyectos.jsx
@@ -7,28 +7,30 @@ export default class Proyectos extends Component{
     render(){
         return(
             <section id="proyectos" className="container__proyectos">
-                <h4 className="proyectos__titulo">Algunos de mis proyectos</h4> 
+                <h4 className="proyectos__titulo">Álgunos de mis proyectos</h4> 
                 <p className="proyectos__parrafo">
                     En mi <a href="https://github.com/baiest" 
-                    target="_blank" rel="noreferrer">Github</a> 
-                    encontraras otros repositorios en los que subo proyectos para practicar,
-                y en los que pruebo nuevas técnologias y lenguajes </p> 
+                    target="_blank" rel="noreferrer">Github</a> encontrarás otros repositorios 
+                    en los que subo proyectos para practicar, 
+                    pruebo nuevas técnologias y lenguajes </p> 
                 
-                {data.map(img => {
+                <div className="container__proyectos--scroll">
+                    {data.map((img, key) => {
                         let src_imagen;
-                        //Revisar si la imagen existe
-                        try {
-                            src_imagen = require(`../assets/${img.nombre_imagen}`).default;
-                        } catch{
-                            src_imagen = '';
-                        }
-                        return(
-                        <TarjetaProyecto imagen={src_imagen}
-                        subtitulo={img.subtitulo}
-                        texto={img.texto}
-                        />)
-
-                    })}
+                            //Revisar si la imagen existe
+                            try {
+                                src_imagen = require(`../assets/${img.nombre_imagen}`).default;
+                            } catch{
+                                src_imagen = '';
+                            }
+                            return(
+                                <TarjetaProyecto key={key} imagen={src_imagen}
+                            subtitulo={img.subtitulo}
+                            texto={img.texto}
+                            />)
+                            
+                        })}
+                </div>
             </section>
         );
     }
